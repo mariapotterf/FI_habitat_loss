ed59b4f7824f1ace2ed2eb93074030c72ac70ad0
Potterf <ge45lep@mytum.de>
2022-03-15 16:36:10 +0100

    Specify individual steps of the project

---
+++ n1_prepare_data.R
@@ -0,0 +1,32 @@
+
+
+# Valuable habitats loss in Finland
+
+# RQ: are we loosing teh most valuable habitats?
+# eg. is the loss faster in more valuable stands?
+# are more valuable stands also older?   
+
+# Steps - develop over individual scripts
+
+
+# Aim: evelop the input data for Pihla for following processing 
+
+# Create tile for the forest age: 2015 - make a raster mosaics from tiles
+#   download data from LUKE: 
+#   manually, from 2015: https://kartta.luke.fi/opendata/valinta-en.html  
+# Analyse the disturbance data - Finland from Cornelius:
+#   dopwnload from: https://zenodo.org/record/4746129#.YjCu5zGZMuU
+# analyse them by the grid of different size? I suggest yes
+# Make a script to get the hexagons over FI
+#       12 km size
+# get map for FI
+
+# Create a forest raster for whole Finland:
+#   read files in zip files
+#   create a mosaic
+# resample to 30 m res?
+# export as single raster: try to shhring down the size
+
+
+
+
