7cce6cf355cdb2791967b7af2975ecf19802d2c4
Potterf <ge45lep@mytum.de>
2022-03-16 09:37:56 +0100

    loop to unzip all files and export as tiffs

--- n1_prepare_data.R
+++ n1_prepare_data.R
@@ -21,12 +21,50 @@
 #       12 km size
 # get map for FI
 
+
+
+# --------------------------------------------------
 # Create a forest raster for whole Finland:
-#   read files in zip files
+# --------------------------------------------------
+
+#   read files in zip files - 'walk' in tidyverse
 #   create a mosaic
-# resample to 30 m res?
-# export as single raster: try to shhring down the size
+#   resample to 30 m res?
+#   export as single raster: try to shhring down the size
+
+library(tidyverse)  # to unzip all files 
+
+
+library(terra)  # for raster processing
 
 
 
+# Read files
+setwd('C:/Users/ge45lep/Documents/2022_disturba_Finland/')
 
+# 
+in_zip  = paste(getwd(), 'raw/FI_age_2015/2015/', sep = '/')
+out_zip = 'C:/Users/ge45lep/Documents/2022_disturba_Finland/raw/age_tif/'
+
+# Read all files in directory
+ls_zip <- list.files(in_zip, pattern = ".zip$", recursive = TRUE)
+
+# walk throught all zip files and extract them all in respectives folders
+walk(ls_zip, ~ unzip(zipfile = str_c(in_zip, .x), 
+                         exdir = str_c(out_zip, .x)))
+
+
+# Read all new raster files, stored in the subdirectories
+ls_tiff <- list.files(out_zip, pattern = ".tif$", recursive = TRUE)  # recursive = True goes into subdirectories
+
+# Read data as rasters
+ls_rst <- lapply(ls_tiff, rast)
+
+
+# Create a raster mosaic of all the rasters:
+# merge the tiles: from the LUKE map, the tiles are 3*12:
+r1 <- rast(paste(out_zip, 'ika_vmi1x_1216_L3.tif.zip/ika_vmi1x_1216_L3.tif', sep = ''))
+
+
+
+r1 <- rast('ika_vmi1x_1216_S4.tif.zip/ika_vmi1x_1216_S4.tif')
